5fe52c6a8079ee9994a9472f25f938e73a7878ab
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2022-03-12 14:20:47 +0300

    first commit

---
+++ GBGoogleMapApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GBGoogleMapApp
+//
+//  Created by Владимир on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let app = "AIzaSyD05zpZsg6DH45dHPPQMBheAL5LXUDh8A8"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
